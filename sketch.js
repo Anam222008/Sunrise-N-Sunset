1f7cfdc1a2f2401fc771cba3b91897e7638173f9
Anam222008 <76837788+Anam222008@users.noreply.github.com>
2021-06-26 16:38:14 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,78 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var backgroundImg;
+
+var bg ;
+
+function preload() {
+    // create getBackgroundImg( ) here
+getBackgroundImg()
+
+}
+
+function setup(){
+    var canvas = createCanvas(1200,700);
+    engine = Engine.create();
+    world = engine.world;
+
+}
+
+function draw(){
+
+    // add condition to check if any background image is there to add
+if(backgroundImg)
+background(backgroundImg)
+
+    Engine.update(engine);
+
+    // write code to display time in correct format here
+fill("black")
+textSize(34)
+if(hour>=12){
+    text("time: "+hour%12+"PM",50,100)
+
+}
+
+else if (hour==0){
+text("time: 12AM",100,100)
+}
+
+else {
+    text("time: "+hour%12+"AM",50,100)
+}
+
+
+}
+
+async function getBackgroundImg(){
+
+    // write code to fetch time from API
+ var reponse = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata")
+    //change the data in JSON format
+var reponseJSON = await reponse.json()
+    // write code slice the datetime
+var datetime = reponseJSON.datetime
+var hour = datetime.slice(11,13)
+//http://worldtimeapi.org/api/timezone/Asia/Kolkata
+    // add conditions to change the background images from sunrise to sunset
+if(hour>=04 && hour<=06){
+    bg = "sunrise1.png";
+}else if(hour>=06 && hour<=08){
+bg = "sunrise2.png";
+}else if(hour>=23 && hour==0){
+bg = "senset10.png";
+}else if(hour==0 && hour<=03){
+    bg = "senset11.png";
+}else{
+    bg = "sunset12.png";
+}
+ 
+
+
+    //load the image in backgroundImg variable here
+backgroundImg = loadImage(bg);
+}
